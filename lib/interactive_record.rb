e0f226006a987c16fadd03a9ee1d2e60a6da9df2
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-17 23:42:01 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -65,7 +65,7 @@ class InteractiveRecord
   def self.find_by(attribute={})
     sql = <<-SQL
       SELECT * FROM #{self.table_name}
-      WHERE #{attribute.keys.to_s.delete(':')} = '#{attribute.values.to_s}''
+      WHERE #{attribute.keys.to_s.delete(':')} = '#{attribute.values.to_s}'
     SQL
 
     DB[:conn].execute(sql)
