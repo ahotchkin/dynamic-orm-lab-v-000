0c775f98b23e87865f13130127e70527cbf18815
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-17 23:14:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -65,7 +65,7 @@ class InteractiveRecord
   def self.find_by(attribute={})
     sql = <<-SQL
       SELECT * FROM #{self.table_name}
-      WHERE '#{attribute.to_i}' = '#{attribute.to_i}'
+      WHERE '#{attribute}' = '#{attribute}'
     SQL
 
     DB[:conn].execute(sql)
