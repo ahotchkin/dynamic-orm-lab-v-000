4522c1999c11590c69cda996f075b14a8b8b6c45
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-17 23:36:52 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -62,7 +62,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(attribute:)
+  def self.find_by(attribute)
     # if attribute.is_a? Integer
     #   sql = <<-SQL
     #     SELECT * FROM #{self.table_name}
